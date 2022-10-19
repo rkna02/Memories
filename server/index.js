79e810fda2d5e36cee7d3ac2f513d55ad1b9e2a5
Peter Na <ruikangna0605@hotmail.com>
2022-10-19 00:03:40 -0700

    fix base url

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/posts', postRoutes);
+
 const CONNECTION_URL = 'mongodb+srv://Walterlee8351:Lufei8351@cluster0.inmtg5e.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
