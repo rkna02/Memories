73516c232b0d9661446949ebbbe2de82f04d14ae
Peter Na <ruikangna0605@hotmail.com>
2022-10-18 23:56:24 -0700

    post and get endpoint for posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,10 +5,18 @@ export const getPosts = async (req, res) => {
         const postMessages = await PostMessage.find();
         res.status(200).json(postMessages);
     } catch (error) {
-        res.status(404).json({message: error.message});
+        res.status(404).json({ message: error.message }); //{"message": "{error.message}"}
     }
 }
 
-export const createPost = (req, res) => {
+export const createPost = async (req, res) => {
+    const post = req.body;
 
+    const newPost = new PostMessage(post);
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message }); //{"message": "{error.message}"}
+    }
 }
